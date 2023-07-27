c946c590a495dcd43705a59d057097a3d5c90efa
lucas-soaresdev <131222060+lucas-soaresdev@users.noreply.github.com>
2023-07-27 20:52:24 -0300

    adicionar seção de eventos

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -24,7 +24,16 @@ export default function Home(){
                 </div>
             </section>
 
-            <section className="box-eventos"></section>
+            <section className="box-eventos">
+                <div>
+                    <h2>nossos eventos</h2>
+                    <span>Delicias com sorvete!</span>
+                    <p>Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui prontinhos para te atender e oferecer os melhores eventos com os melhores sorvete da sua vida! Venha nos conhecer e passar um tempo aqui com a gente.</p>
+                </div>
+                <div>
+                    <img src="./assets/eventos-image.jpg"/>
+                </div>
+            </section>
 
             <section className="box-"></section>
         </main>
