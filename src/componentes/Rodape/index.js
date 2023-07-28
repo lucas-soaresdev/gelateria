3677337f82322439c04c938b06c5e0f272118952
lucas-soaresdev <131222060+lucas-soaresdev@users.noreply.github.com>
2023-07-27 21:04:24 -0300

    adição do rodapé e seção sobre

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Rodape(){
+    return (
+        <footer>
+            <div>
+                <img src=""/>
+                <div>
+                    <h3>endereço</h3>
+                    <p>Av. Bernardino de Campos, 98</p>
+                    <p>São Paulo, SP 12345-678</p>
+                </div>
+                <div>
+                    <h3>contato</h3>
+                    <p>info@meusite.com</p>
+                    <p>Tel: (11) 3456-7890</p>
+                </div>
+                <div>
+                    <h3>horários</h3>
+                    <p>ABERTO TODOS OS DIAS</p>
+                    <p>10:00 - 22:00</p>
+                </div>
+            </div>
+            <div>
+                <p>Gelateria. Orgulhosamente desenvolvido por <span>Lucas Soares</span></p>
+            </div>
+        </footer>
+    )
+}
--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Topo from '../../componentes/Topo'
+import Rodape from '../../componentes/Rodape';
 
 export default function Home(){
     return (
@@ -35,8 +36,18 @@ export default function Home(){
                 </div>
             </section>
 
-            <section className="box-"></section>
+            <section className="box-Sobre">
+                <div>
+                    <img src="./assets/sobre-image.jpg"/>
+                </div>
+                <div>
+                    <h2>sobre nós</h2>
+                    <span>Alegria em cada casquinha!</span>
+                    <p>Venha tomar o melhor sorvete da região aqui com a gente! Nós estamos há anos no mercado produzindo o que tem de melhor para o 	nosso cliente e você não pode ficar fora dessa. Venha nos fazer uma visita e aproveite o melhor atendimento e o melhor sorvete da cidade.</p>
+                </div>
+            </section>
         </main>
+        <Rodape />
         </div>
     )
 }
