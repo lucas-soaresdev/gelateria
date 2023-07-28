16737b3cf8d1b557b9ae281ebf51d2107698f0ad
lucas-soaresdev <131222060+lucas-soaresdev@users.noreply.github.com>
2023-07-28 06:06:47 -0300

    ajuste no responsivo

--- src/paginas/Sobre/index.js
+++ src/paginas/Sobre/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './style.css'
 
 import Topo from '../../componentes/Topo'
 import Rodape from '../../componentes/Rodape'
@@ -8,16 +9,16 @@ export default function Sobre(){
         <div>
             <Topo />
             <main>
-                <section>
+                <section className="box-banner-sobre">
                     <h1>a gelateria</h1>
                 </section>
-                <section>
+                <section className="box-sobre-nos">
                     <h2>Sobre Nós</h2>
                     <span>Nós simplesmente amamos sorvete!</span>
                     <p>Somos uma empresa apaixonada pelo que faz. Colocamos amor em cada sorvete produzido. Fazemos o melhor sorvete para os nossos clientes e gostamos de receber elogios. Estamos operando desde 2009 com as melhores matérias-primas para a produção final do sorvete. Vendemos tanto para varejo como para atacado.</p>
                     <p>Nossos clientes podem comprar os nossos sorvetes e degustar na nossa loja ou levar para sua residência e aproveitar junto com a família. Também vendemos para estabelecimentos e criamos eventos como festa de aniversário, formaturas e eventos empresariais. Para contratar os nossos serviços, basta entrar em contato conosco. Iremos proporcionar o melhor atendimento e os melhores produtos para você fazer a sua festa mais saborosa, com o melhor sorvete da cidade.</p>
                 </section>
-                <section>
+                <section className="box-imagens">
                     <div>
                         <img src="./assets/eventos-image.jpg"/>
                     </div>
