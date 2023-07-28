2295a67a4e84b2efbaf1556d2a828c86ef741e5a
lucas-soaresdev <131222060+lucas-soaresdev@users.noreply.github.com>
2023-07-27 22:17:26 -0300

    estilização rodapé

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -3,26 +3,26 @@ import './style.css'
 
 export default function Rodape(){
     return (
-        <footer>
-            <div>
-                <img src=""/>
-                <div>
+        <footer className="rodape">
+            <div className="container-rodape limitar-secao">
+                <img src="./assets/logo.png"/>
+                <div className="cards">
                     <h3>endereço</h3>
                     <p>Av. Bernardino de Campos, 98</p>
                     <p>São Paulo, SP 12345-678</p>
                 </div>
-                <div>
+                <div className="cards">
                     <h3>contato</h3>
                     <p>info@meusite.com</p>
                     <p>Tel: (11) 3456-7890</p>
                 </div>
-                <div>
+                <div className="cards">
                     <h3>horários</h3>
                     <p>ABERTO TODOS OS DIAS</p>
                     <p>10:00 - 22:00</p>
                 </div>
             </div>
-            <div>
+            <div className="container-creditos">
                 <p>Gelateria. Orgulhosamente desenvolvido por <span>Lucas Soares</span></p>
             </div>
         </footer>
