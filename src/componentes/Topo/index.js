dd67034c691bc790d96c9e5cda0396e18cb9dd96
lucas-soaresdev <131222060+lucas-soaresdev@users.noreply.github.com>
2023-07-27 21:20:07 -0300

    estilização do topo

--- src/componentes/Topo/index.js
+++ src/componentes/Topo/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import './style.css'
 
 export default function Topo(){
     return (
         <header className="topo">
-            <nav className="nav-bar">
+            <nav className="nav-bar limitar-secao">
                 <div className="container-logo">
                     <img src="./assets/logo.png" />
                 </div>
--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './style.css'
 
 import Topo from '../../componentes/Topo'
 import Rodape from '../../componentes/Rodape';
