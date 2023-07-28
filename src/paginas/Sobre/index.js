6073084b3eed8bbf78844bb383479ebaa693ff80
lucas-soaresdev <131222060+lucas-soaresdev@users.noreply.github.com>
2023-07-28 06:49:50 -0300

    estilização e responsivo da página sobre

--- src/paginas/Sobre/index.js
+++ src/paginas/Sobre/index.js
@@ -13,18 +13,16 @@ export default function Sobre(){
                     <h1>a gelateria</h1>
                 </section>
                 <section className="box-sobre-nos">
-                    <h2>Sobre Nós</h2>
-                    <span>Nós simplesmente amamos sorvete!</span>
-                    <p>Somos uma empresa apaixonada pelo que faz. Colocamos amor em cada sorvete produzido. Fazemos o melhor sorvete para os nossos clientes e gostamos de receber elogios. Estamos operando desde 2009 com as melhores matérias-primas para a produção final do sorvete. Vendemos tanto para varejo como para atacado.</p>
-                    <p>Nossos clientes podem comprar os nossos sorvetes e degustar na nossa loja ou levar para sua residência e aproveitar junto com a família. Também vendemos para estabelecimentos e criamos eventos como festa de aniversário, formaturas e eventos empresariais. Para contratar os nossos serviços, basta entrar em contato conosco. Iremos proporcionar o melhor atendimento e os melhores produtos para você fazer a sua festa mais saborosa, com o melhor sorvete da cidade.</p>
+                    <div className="limitar-secao">
+                        <h2>Sobre Nós</h2>
+                        <span>Nós simplesmente amamos sorvete!</span>
+                        <p>Somos uma empresa apaixonada pelo que faz. Colocamos amor em cada sorvete produzido. Fazemos o melhor sorvete para os nossos clientes e gostamos de receber elogios. Estamos operando desde 2009 com as melhores matérias-primas para a produção final do sorvete. Vendemos tanto para varejo como para atacado.</p>
+                        <p>Nossos clientes podem comprar os nossos sorvetes e degustar na nossa loja ou levar para sua residência e aproveitar junto com a família. Também vendemos para estabelecimentos e criamos eventos como festa de aniversário, formaturas e eventos empresariais. Para contratar os nossos serviços, basta entrar em contato conosco. Iremos proporcionar o melhor atendimento e os melhores produtos para você fazer a sua festa mais saborosa, com o melhor sorvete da cidade.</p>
+                    </div>
                 </section>
                 <section className="box-imagens">
-                    <div>
-                        <img src="./assets/eventos-image.jpg"/>
-                    </div>
-                    <div>
+                        <img src="./assets/sobre-image.jpg"/>
                         <img src="./assets/sorveteria.jpg"/>
-                    </div>
                 </section>
             </main>
             <Rodape />
