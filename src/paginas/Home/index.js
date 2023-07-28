a335a521dfb4e98434876ff6cbae6f5a026d90a3
lucas-soaresdev <131222060+lucas-soaresdev@users.noreply.github.com>
2023-07-27 21:57:27 -0300

    estlização das seções: sabores, eventos e sobre.

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -32,16 +32,16 @@ export default function Home(){
                     <span>Delicias com sorvete!</span>
                     <p>Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui prontinhos para te atender e oferecer os melhores eventos com os melhores sorvete da sua vida! Venha nos conhecer e passar um tempo aqui com a gente.</p>
                 </div>
-                <div>
+                <div className="container-imagem">
                     <img src="./assets/eventos-image.jpg"/>
                 </div>
             </section>
 
-            <section className="box-Sobre">
-                <div>
+            <section className="box-sobre">
+                <div className="container-imagem">
                     <img src="./assets/sobre-image.jpg"/>
                 </div>
-                <div>
+                <div className="container-texto">
                     <h2>sobre nós</h2>
                     <span>Alegria em cada casquinha!</span>
                     <p>Venha tomar o melhor sorvete da região aqui com a gente! Nós estamos há anos no mercado produzindo o que tem de melhor para o 	nosso cliente e você não pode ficar fora dessa. Venha nos fazer uma visita e aproveite o melhor atendimento e o melhor sorvete da cidade.</p>
