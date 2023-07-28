2b28e6cb5bd302323dbc15e79a96bd6e42208664
lucas-soaresdev <131222060+lucas-soaresdev@users.noreply.github.com>
2023-07-28 02:58:59 -0300

    estilização e responsivo da página sabores

--- src/paginas/Sabores/index.js
+++ src/paginas/Sabores/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './style.css'
 
 import Topo from '../../componentes/Topo'
 import Rodape from '../../componentes/Rodape'
@@ -8,39 +9,39 @@ export default function Sabores(){
         <div>
             <Topo />
             <main>
-                <section>
+                <section className="box-banner-sabores">
                     <h1>nossos sabores</h1>
                 </section>
-                <section>
+                <section className="box-sabores-cards">
                     <h2>sabores de sorvete</h2>
-                    <div>
-                        <div>
-                            <img src="./assets/sabor-oreo.jpg"/>
+                    <div className="limitar-secao sabores">
+                        <div className="cards">
+                            <div className="container-imagem"><img src="./assets/sabor-oreo.jpg"/></div>
                             <h3>Sorvete de oreo</h3>
                             <p>Delicioso sorvete sabor Oreo. Uma explosão de sabor.</p>
                         </div>
-                        <div>
-                            <img src="./assets/sabor-pistache.png"/>
+                        <div className="cards">
+                            <div className="container-imagem"><img src="./assets/sabor-pistache.png"/></div>
                             <h3>Sorvete Pistache</h3>
                             <p>Cremoso sorvete sabor pistache com pedacinhos de semente.</p>
                         </div>
-                        <div>
-                            <img src="./assets/sabor-cookies-avela.png"/>
+                        <div className="cards">
+                            <div className="container-imagem"><img src="./assets/sabor-cookies-avela.png"/></div>
                             <h3>Sorvete Cookies & Avelã</h3>
                             <p>O nosso melhor sorvete. Você vai adorar o sabor.</p>
                         </div>
-                        <div>
-                            <img src="./assets/sorbet-kiwi.png"/>
+                        <div className="cards">
+                            <div className="container-imagem"><img src="./assets/sorbet-kiwi.png"/></div>
                             <h3>Sorvete de Kiwi</h3>
                             <p>Delicioso e refrescante sorvete sabor kiwi. Rico em vitamina C.</p>
                         </div>
-                        <div>
-                            <img src="./assets/sorbet-morango.png"/>
+                        <div className="cards">
+                            <div className="container-imagem"><img src="./assets/sorbet-morango.png"/></div>
                             <h3>Sorvete de Morango</h3>
                             <p>Sorvete de morango gourmet. Tradicional e saboroso.</p>
                         </div>
-                        <div>
-                            <img src="./assets/sorbet-limao.png"/>
+                        <div className="cards">
+                            <div className="container-imagem"><img src="./assets/sorbet-limao.png"/></div>
                             <h3>Sorvete de Limão Siciliano</h3>
                             <p>O incrivel sorvete gourmet de limão siciliano vai te encantar.</p>
                         </div>
